73f7f7aed6089a4e7b367236b23d2d06b306d989
Joonyoung Im <troy51538@gmail.com>
2024-06-26 12:45:32 -0400

    searchbar for ios

--- src/features/map/components/search.component.js
+++ src/features/map/components/search.component.js
@@ -1,40 +1,36 @@
 import React, { useContext, useState, useEffect } from "react";
-import { SafeAreaView } from "react-native";
-import { Searchbar } from "react-native-paper";
 import styled from "styled-components/native";
+import { Searchbar } from "react-native-paper";
+
 import { LocationContext } from "../../../services/location/location.context";
 
-const SearchComponent = styled.View`
-  padding: ${(props) => props.theme.sizes[3]};
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
   position: absolute;
   z-index: 999;
-  top: 30px;
+  top: 25px;
   width: 100%;
 `;
 
 export const Search = () => {
   const { keyword, search } = useContext(LocationContext);
-  const [searchKeyword, setSearchkeyword] = useState(keyword);
-
+  const [searchKeyword, setSearchKeyword] = useState(keyword);
   useEffect(() => {
-    setSearchkeyword(keyword);
+    setSearchKeyword(keyword);
   }, [keyword]);
-
   return (
-    <SafeAreaView>
-      <SearchComponent>
-        <Searchbar
-          icon="map"
-          placeholder="Search for a location"
-          value={searchKeyword}
-          onSubmitEditing={() => {
-            search(searchKeyword);
-          }}
-          onChangeText={(text) => {
-            setSearchkeyword(text);
-          }}
-        />
-      </SearchComponent>
-    </SafeAreaView>
+    <SearchContainer>
+      <Searchbar
+        placeholder="Search for a location"
+        icon="map"
+        value={searchKeyword}
+        onSubmitEditing={() => {
+          search(searchKeyword);
+        }}
+        onChangeText={(text) => {
+          setSearchKeyword(text);
+        }}
+      />
+    </SearchContainer>
   );
 };
